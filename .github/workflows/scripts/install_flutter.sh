ba981b5bf2bc89d14979e02b914a17cc2d4d6a71
abolfazl abasi <87444842+AbolfazlAbbasiSaeb@users.noreply.github.com>
2025-10-26 12:18:09 +0330

    uo

--- .github/workflows/scripts/install_flutter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable _flutter
-echo "$GITHUB_WORKSPACE/_flutter/bin" >> "$GITHUB_PATH"
-
-cd "$GITHUB_WORKSPACE/_flutter/bin"
-./flutter doctor
